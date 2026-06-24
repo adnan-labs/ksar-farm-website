300b29dfac870bd87dfd89637c0a48ebf48cf443
Adnan <ashaikh7187@gmail.com>
2026-06-24 18:50:28 +0530

    Update website title

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-/* =============================================
-   UPDATED APP.JS WITH ALL ROUTES
-   Place this in: src/App.js
-   ============================================= */
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Bhimashankar from './pages/Bhimashankar';
 import SahyadriRanges from './pages/SahyadriRanges';
 import SidhagadFort from './pages/SidhagadFort';
 import Waterfalls from './pages/Waterfalls';
 
-
-
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
-        {/* Header on all pages */}
         <Header />
-
-        {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bhimashankar" element={<Bhimashankar />} />
@@ -31,8 +23,6 @@ function App() {
           <Route path="/sidhagad" element={<SidhagadFort />} />
           <Route path="/waterfalls" element={<Waterfalls />} />
         </Routes>
-
-        {/* Footer on all pages */}
         <Footer />
       </div>
     </Router>
